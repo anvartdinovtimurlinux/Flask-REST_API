6e8126fb0a79878d068a6c5452f13d361247d610
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-06-25 09:30:24 +0300

    fix bug with method PUT

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 def load_users(path_to_file):
     with open(path_to_file, 'r', encoding='utf-8') as f:
-        return json.load(f)
+        try:
+            data = json.load(f)
+        except ValueError:
+            data = {}
+        return data
 
 
 def save_users(path_to_file, data):
@@ -28,7 +32,12 @@ def create_user_dict(name, login, password=None):
 
 @app.route('/user/', methods=['GET'])
 def get_users():
-    return load_users('user.json'), 200
+    users = load_users('user.json')
+    users_without_password = [{
+        'login': user['login'],
+        'name': user['name']
+    } for user in users.values()]
+    return jsonify(users_without_password), 200
 
 
 @app.route('/user/<user_id>', methods=['GET'])
@@ -47,21 +56,24 @@ def get_user(user_id):
 def create_user():
     users = load_users('user.json')
 
-    if not request.json['login'] or not request.json.get('password'):
+    if not request.json.get('login') or not request.json.get('password'):
         return jsonify({'error': 'Invalid user data'}), 400
-    elif request.json['login'] in [user['login'] for user in users.values()]:
+    elif request.json.get('login') in [user['login'] for user in users.values()]:
         return jsonify({'error': 'User already exist'}), 400
 
     user = create_user_dict(
+        request.json.get('name', request.json.get('login')),
         request.json.get('login'),
-        request.json.get('password'),
-        request.json.get('name', request.json.get('login'))
+        request.json.get('password')
     )
-    user_id = str(max(int(key) for key in users.keys()) + 1)
+    if len(users):
+        user_id = str(max(int(key) for key in users.keys()) + 1)
+    else:
+        user_id = 0
     users[user_id] = user
 
     save_users('user.json', users)
-    return jsonify({'user_id': user_id}), 200
+    return jsonify({'user_id': user_id}), 201
 
 
 @app.route('/user/<user_id>', methods=['PUT'])
@@ -72,19 +84,20 @@ def update_user(user_id):
         return jsonify({'error': 'User not found'}), 404
     elif not request.json:
         return jsonify({'error': 'No new user information'}), 400
-    elif request.json['login'] in [user['login'] for user in users.values()]:
+    elif request.json.get('login') in [user['login'] for user in users.values()]:
         return jsonify({'error': 'This login already exists'}), 400
 
     user = create_user_dict(
+        request.json.get('name', users[user_id]['name']),
         request.json.get('login', users[user_id]['login']),
-        request.json.get('password'),
-        request.json.get('name')
+        request.json.get('password')
     )
     users[user_id].update(user)
     save_users('user.json', users)
 
-    user.pop('password')
-    return jsonify(user), 201
+    if user.get('password'):
+        user.pop('password')
+    return jsonify(user), 200
 
 
 @app.route('/user/<user_id>', methods=['DELETE'])
